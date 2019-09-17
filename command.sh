a00bfbb15c14d313b49087be8fc3cf423a91d7ed
Suraj Agarkar <46438520+agarkarsuraj1@users.noreply.github.com>
2019-09-17 12:50:34 +0530

    Update command.sh
    remove folder content

--- command.sh
+++ command.sh
@@ -3,7 +3,7 @@ result="$?"
 if [ "$result" -eq "1" ]
 then
 	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", 	"text": "'${PWD##*/}' pull request failed the unit testing and it was rejected", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
-	#rm -rf 'la -Al'
+	rm -rf 'la -Al'
 else
 	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", "text": " '${PWD##*/}' pull request passed the test", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
 	
