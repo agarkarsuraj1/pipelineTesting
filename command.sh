37ab411a64a32f1437032b4ed43a2082ae6e97c4
Suraj Agarkar <46438520+agarkarsuraj1@users.noreply.github.com>
2019-09-17 12:55:03 +0530

    Update commnand.sh
    Changes of rm command

--- command.sh
+++ command.sh
@@ -2,8 +2,9 @@ npm install
 result="$?"
 if [ "$result" -eq "1" ]
 then
-	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", 	"text": "'${PWD##*/}' pull request failed the unit testing and it was rejected", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
 	rm -rf 'la -Al'
+	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", 	"text": "'${PWD##*/}' pull request failed the unit testing and it was rejected", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
+	
 else
 	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", "text": " '${PWD##*/}' pull request passed the test", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
 	
