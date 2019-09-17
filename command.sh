d564bba03bafde2d6dbb8ebc073ae67c173967b9
Suraj Agarkar <suraj.agarkar@roxai.com>
2019-09-17 11:15:41 +0530

    Testing Pipeline

---
+++ command.sh
@@ -0,0 +1,11 @@
+npm install
+result="$?"
+if [ "$result" -eq "127" ]
+then
+	curl -X POST -H 'application/json' -d '{"channel": "##devops", "username": "Devops Team", 	"text": "Scheduler pull request failed the unit testing and it was rejected", "icon_emoji": 	":muscle:"}' https://hooks.slack.com/services/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
+	#rm -rf 'la -Al'
+else
+	curl -X POST -H 'application/json' -d '{"channel": "##devops", "username": "Devops Team", 	"text": "Scheduler pull request passed the test", "icon_emoji": ":muscle:"}' 			https://hooks.slack.com/services/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
+	
+fi
+
