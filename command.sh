8f4ddd692b7f18d3f9ea8520c3faa8b6226bdd6a
Suraj Agarkar <46438520+agarkarsuraj1@users.noreply.github.com>
2019-09-17 14:15:50 +0530

    Update command.sh
    rm *

--- command.sh
+++ command.sh
@@ -2,7 +2,7 @@ npm install
 result="$?"
 if [ "$result" -eq "1" ]
 then
-	rm -rf 'la -Al'
+	rm *
 	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", 	"text": "'${PWD##*/}' pull request failed the unit testing and it was rejected", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
 	
 else
