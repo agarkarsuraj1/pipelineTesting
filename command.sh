c4d4b910bea1f1a1dd262ae3b818ec7f9ccc59f9
Suraj Agarkar <46438520+agarkarsuraj1@users.noreply.github.com>
2019-09-17 12:45:01 +0530

    Update command.sh
    update changes

--- command.sh
+++ command.sh
@@ -1,11 +1,10 @@
-npm installl
+npm install
 result="$?"
 if [ "$result" -eq "1" ]
 then
-	curl -X POST -H 'application/json' -d '{"channel": "##devops", "username": "Devops Team", 	"text": "pull request failed the unit testing and it was rejected", "icon_emoji": 	":muscle:"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", 	"text": "'${PWD##*/}' pull request failed the unit testing and it was rejected", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
 	#rm -rf 'la -Al'
 else
-	curl -X POST -H 'application/json' -d '{"channel": "##devops", "username": "Devops Team", "text": "pull request passed the test", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'application/json' -d '{"channel": "##testcommand", "username": "Devops Team", "text": " '${PWD##*/}' pull request passed the test", "icon_emoji": ":muscle:"}' https://hooks.slack.com/services/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
 	
 fi
-
